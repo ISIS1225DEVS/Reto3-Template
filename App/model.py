19aff10ed8aea2fdae5e6e1d37e939bdebbe96f9
Moises Agudelo <m.agudeloo@uniandes.edu.co>
2021-10-27 16:06:55 -0500

    Primera entrega – Reto 3

--- App/model.py
+++ App/model.py
@@ -24,27 +24,158 @@
  * Dario Correal - Version inicial
  """
 
+#•••••••••••••••••••••••••••••••••••••••••
+#   Importaciones
+#•••••••••••••••••••••••••••••••••••••••••
 
 import config as cf
 from DISClib.ADT import list as lt
 from DISClib.ADT import map as mp
 from DISClib.DataStructures import mapentry as me
 from DISClib.Algorithms.Sorting import shellsort as sa
+from DISClib.ADT import orderedmap as om
 assert cf
+import datetime
+from DISClib.ADT import map as m
 
-"""
-Se define la estructura de un catálogo de videos. El catálogo tendrá dos listas, una para los videos, otra para las categorias de
-los mismos.
-"""
 
-# Construccion de modelos
 
+
+
+#•••••••••••••••••••••••••••••••••••••••••
+#   Inicializacion del catalogo
+#•••••••••••••••••••••••••••••••••••••••••
+
+def newAnalyzer():
+    """ Inicializa el analizador
+    Crea una lista vacia para guardar todos los crimenes
+    Se crean indices (Maps) por los siguientes criterios:
+    -Fechas
+    Retorna el analizador inicializado.
+    """
+    
+    #analyzer['dateIndex'] = om.newMap(omaptype='RBT',
+     #                                 comparefunction=compareDates)
+    
+    analyzer = mp.newMap(
+                        2,
+                        maptype = "CHAINING",
+                        loadfactor = 4.0,
+                        comparefunction = None
+                    )
+
+    mp.put(
+            analyzer,
+            "cases",
+            lt.newList('ARRAY_LIST')
+            )
+
+    mp.put(
+            analyzer,
+            "casesSize",
+            lt.size(me.getValue(mp.get(analyzer, "cases"))) 
+            )
+
+    return analyzer
+
+
+
+
+#•••••••••••••••••••••••••••••••••••••••••
+#Funciones de consulta
+#•••••••••••••••••••••••••••••••••••••••••
+
+def getCasesByCity(analyzer, city):
+
+    """
+    
+        Responde al requerimiento 1.
+
+    """
+
+    return None
+
+#----------------------------------------
+
+def getCasesBetweeenSeconds(analyzer, beginSeconds, endSeconds):
+
+    """
+    
+        Responde al requerimiento 2.
+
+    """
+
+    return None
+
+#----------------------------------------
+
+def getCasesBetweeenHours(analyzer, beginHour, endHour):
+
+    """
+    
+        Responde al requerimiento 3.
+
+    """
+
+    return None
+
+
+
+
+
+#•••••••••••••••••••••••••••••••••••••••••
 # Funciones para agregar informacion al catalogo
+#•••••••••••••••••••••••••••••••••••••••••
 
+def addCase(analyzer, case):
+    """
+    """
+    lt.addLast(me.getValue(mp.get(analyzer, "cases")), case)
+    return analyzer
+
+
+
+
+
+
+#•••••••••••••••••••••••••••••••••••••••••
 # Funciones para creacion de datos
+#•••••••••••••••••••••••••••••••••••••••••
 
-# Funciones de consulta
 
-# Funciones utilizadas para comparar elementos dentro de una lista
 
+
+
+#•••••••••••••••••••••••••••••••••••••••••
+# Funciones adicionales
+#•••••••••••••••••••••••••••••••••••••••••
+
+def getCasesSize(analyzer):
+    return lt.size(me.getValue(mp.get(analyzer, "cases")))
+
+
+
+#•••••••••••••••••••••••••••••••••••••••••
+# Funciones de comparacion
+#•••••••••••••••••••••••••••••••••••••••••
+
+def compareIds(id1, id2):
+    """
+    Compara dos crimenes
+    """
+    if (id1 == id2):
+        return 0
+    elif id1 > id2:
+        return 1
+    else:
+        return -1
+
+#----------------------------------------
+
+
+
+
+
+#•••••••••••••••••••••••••••••••••••••••••
 # Funciones de ordenamiento
+#•••••••••••••••••••••••••••••••••••••••••
--- App/view.py
+++ App/view.py
@@ -23,8 +23,13 @@
 import config as cf
 import sys
 import controller
-from DISClib.ADT import list as lt
 assert cf
+from DISClib.ADT import list as lt
+from DISClib.ADT import map as mp
+from DISClib.DataStructures import mapentry as me
+
+ufosFile = 'UFOS//UFOS-utf8-small.csv'
+cont = None
 
 
 """
@@ -35,24 +40,76 @@ operación solicitada
 """
 
 def printMenu():
-    print("Bienvenido")
-    print("1- Cargar información en el catálogo")
-    print("2- ")
+
+    print("\n••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••")
+
+    print("\nBienvenido.\n")
+
+    print("-----------------------------------------------------------------------------")
+
+    print("\n1 - Inicializar el catalogo.\n")
+
+    print("-----------------------------------------------------------------------------")
+
+    print("\n2 - Cargar información en el catalogo.\n")
+
+    print("-----------------------------------------------------------------------------")
+
+    print("\n3 - Requerimiento 1.\n\nMostrar:\n\n•El total de ciudades donde se han presentado avistamientos.\n\n•Un top de 5 ciudades donde mas avistamientos se han presentado\nde manera descendente por criterio de cantidad.\n\n•La cantidad de avistamientos en una ciudad especifica.\n\n•Una lista de los tres avistamientos mas antiguos y los tres\nmas recientes de una ciudad en especifico.\n")
+
+    print("-----------------------------------------------------------------------------")
+
+    print("\n4 - Requerimiento 2.\n\nMostrar:\n\n•Un top de 5 avistamientos de duracion mas larga en segundos de manera\ndescendiente por criterio de duracion junto con el conteo de avistamientos\nque duraron este mismo tiempo.\n\nSobre una intervalo determinado de duracion en segundos,\n\n• La cantidad de avistamientos en ese intertvalo.\n\n•Una lista de los tres avistamientos mas antiguos y los tres mas\nrecientes que se encuentren en ese intervalo.\n")
+
+    print("-----------------------------------------------------------------------------")
+
+    print("\n5 - Requerimiento 3.\n\nMostrar:\n\n• Un top de 5 avistamientos registrados a una hora mas tardia de orden\ndescendiente y con criterio la hora regitrada junto con el conteo\nde avistamientos presentados en esta misma hora.\n\n• El conteo de avistamientos registrados en un intervalo de horas\ndeterminadas.\n\n• Una lista de los 3 avistamientos mas antiguos y de los 3 avistamientos\nmas recientes regitrados en este intervalo de tiempo.\n")
+
+    print("-----------------------------------------------------------------------------")
+
+    print("\nInserte cualquier otro caracter para salir de la aplicacion.\n")
+
+    print("••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••\n")
 
 catalog = None
 
 """
 Menu principal
 """
-while True:
-    printMenu()
-    inputs = input('Seleccione una opción para continuar\n')
-    if int(inputs[0]) == 1:
-        print("Cargando información de los archivos ....")
 
-    elif int(inputs[0]) == 2:
-        pass
+loop = True
+
+while loop:
+    printMenu()
+    inputs = input('Seleccione una opción para continuar: ')
+    if inputs[0] == "1":
+        print("\nInicializando....")
+        cont = controller.init()
+
+    elif inputs[0] == "2":
+        print("\nCargando información de crimenes ....")
+        controller.loadData(cont, ufosFile)
+        
+        print('Total de avistamientos cargados: ' + str(controller.getCasesSize(cont)))
+        print('Primeros 5 avistamientos: ')
+        print('Ultimos 5 avistamientos: ')
+        print(me.getValue(mp.get(cont, "cases")))
+
+    elif inputs[0] == "3":
+        city = input("Inserte la ciudad en la cual desea consultar la información.")
+        print(controller.getCasesByCity(cont, city))
+
+    elif inputs[0] == "4":
+        beginSeconds = input("Inserte la fecha incial en formato AAAA-MM-DD: ")
+        endSeconds = input("Inserte la fecha final en formato AAAA-MM-DD: ")
+        print(controller.getCasesBetweeenSeconds(cont, beginSeconds, endSeconds))
+
+    elif inputs[0] == "5":
+        beginHour = input("Inserte la fecha incial en formato AAAA-MM-DD: ")
+        endHour = input("Inserte la fecha final en formato AAAA-MM-DD: ")
+        print(controller.getCasesBetweeenHours(cont, beginHour, endHour))
+
 
     else:
-        sys.exit(0)
-sys.exit(0)
+        print("\nSaliendo de la aplicacion...\n")
+        loop = False
