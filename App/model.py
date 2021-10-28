9f191ac36102f2a211d007cca0ed600fc2340675
ANGEL COLONIA <a.colonia@uniandes.edu.co>
2021-10-27 23:40:25 -0500

    “Avance Req 1– Reto 3"

--- App/model.py
+++ App/model.py
@@ -39,12 +39,85 @@ los mismos.
 
 # Construccion de modelos
 
+def initCatalog():
+    catalog = {'avistamiento': None,'dateIndex': None}
+
+    catalog['avistamiento'] = lt.newList('SINGLE_LINKED', compare)
+    catalog['dateIndex'] = om.newMap(omaptype='BST', comparefunction=compare)
+    catalog['City']=om.newMap(omaptype='BST',comparefunction=compare)
+    return catalog
+
 # Funciones para agregar informacion al catalogo
+def AddAvist(catalog, avistamiento):
+    lt.addLast(catalog['avistamiento'], avistamiento)
+    City  = avistamiento['City']
+    State = om.contains(catalog['City'], City)
+    if not State:
+        Ltcity = lt.newList()
+        lt.addLast(listaCiudad, avistamiento)
+        om.put(catalog['City'],City,Ltcity)
+    else:
+        Ltcity = om.get(catalog['City'], City)['value']
+        lt.addLast(Ltcity, avistamiento)
+        om.put(catalog['City'], City, Ltcity)
 
 # Funciones para creacion de datos
 
 # Funciones de consulta
+def avistCiudad(catalog, City):
+    NumberOfcities=0
+    NumberAvistCiudad=0
+    cityLt= lt.newList()
+    for City  in lt.iterator(catalog['City']):
+        NumberOfcities+=1
+        if catalog['City'] == City:
+            for i in catalog['City']:
+                NumberAvistCiudad+= 1
+                Dat =lt.newList()
+                lt.addLast(Dat, i['Datetime'])
+                lt.addLast(Dat, i['City'])
+                lt.addLast(Dat, i['Country'])
+                lt.addLast(Dat, i['Duration'])
+                lt.addLast(Dat, i['Shape'])
+                lt.addLast(cityLt, Dat)
+    return cityLt
 
+
+def ltDates(cityLt):
+    ltDates= lt.newList()
+    for i in ltDates:
+        lt.addLast(ltDates, i['Datetime'])
+    return ltDates
+
+def Orderedlt(ltDates):
+    Orderedlt=sa.sort(ltDates, compare)
+    return ltDates
+
+def OrderedArtist(Orderedlt, cityLt ):
+    ordered = lt.newList
+    for date in Orderedlt:
+        for i in cityLt:
+            if date == i['Datetime']:
+                lt.addLast(ordered, i)
+    return ordered
+
+def top3fi(ordered):
+    firsts=lt.subList(ordered, 1, 3)
+    return firsts
+
+def top3la(ordered):
+    latests=lt.subList(ordered, (lt.size(ordered))-2, 3)
+    return latests
 # Funciones utilizadas para comparar elementos dentro de una lista
 
+def compare(v1, v2):
+    if (v1 == v2):
+        return 0
+    elif v1 > v2:
+        return 1
+    else:
+        return -1
+
 # Funciones de ordenamiento
+
+
